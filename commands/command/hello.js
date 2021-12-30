a162c3130ffbd2c91923407734d0bb8cbfeabc86
HeartandSoulOfficial <heartandsoulofficial024@gmail.com>
2021-12-30 15:19:05 -0500

    Ignore Config

--- commands/command/hello.js
+++ commands/command/hello.js
@@ -1,20 +1,3 @@
-/*const Discord = require('discord.js')
-
-module.exports.run = async (client, message, args, prefix) => {
-    if (!message.content.startsWith(prefix)) return;
-    message.channel.send('Hello!')
-}
-
-module.exports.help = {
-    name: 'hello'
-}*/
-
-
-/*module.exports.run = (client, message, prefix) => {
-    if (messgae.content.startsWith(prefix))
-    message.channel.send("Hello!").catch(console.error);
-}*/
-
 const Discord = require('discord.js')
 
 module.exports.run = async (client, message, args, gprefix) => {
