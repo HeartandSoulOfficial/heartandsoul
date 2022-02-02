54f4faaa09eccfb3bd3de3ca43a6bb400b78cfcf
HeartandSoulOfficial <heartandsoulofficial024@gmail.com>
2022-02-02 17:54:05 -0500

    Hide invite link with env

--- commands/command/invite.js
+++ commands/command/invite.js
@@ -1,11 +1,12 @@
 const {discord, MessageEmbed} = require('discord.js')
+require('dotenv').config()
 
 module.exports.run = async (client, message, args, gprefix) => {
     if (message.member.user.id === '274909438366973953' || message.member.user.id === '493163747704045568'){
         message.delete()
         const invite = new MessageEmbed()
-            .setDescription(`[Invite Me!](https://discordapp.com/oauth2/authorize?client_id=920885512208793652&permissions=18432&scope=bot&permissions=2134207679)`)
-        message.channel.send({embeds: [invite]}).then(m => setTimeout(() => m.delete(), 10000))
+            .setDescription(`[Invite Me!](${process.env.INVITE})`)
+        message.channel.send({embeds: [invite]}).then(m => setTimeout(() => m.delete(), 5000))
     }
 }
 
