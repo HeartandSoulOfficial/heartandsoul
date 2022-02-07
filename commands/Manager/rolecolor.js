0ca06371281a5ff144f8321bf022fe9985992b4b
HeartandSoulOfficial <heartandsoulofficial024@gmail.com>
2022-02-07 18:27:59 -0500

    Fixed rolecolor command and added random color

--- commands/Manager/rolecolor.js
+++ commands/Manager/rolecolor.js
@@ -2,7 +2,7 @@ const {discord, MessageEmbed} = require('discord.js')
 
 module.exports.run = async (client, message, args, gprefix) => {
     const unfound = new MessageEmbed()
-        .setDescription("Couldn't find that user.")
+        .setDescription("Couldn't find that role.")
         .setColor('FUCHSIA')
     const perms = new MessageEmbed()
         .setDescription('I don\'t have permissions to change role color.')
@@ -10,6 +10,9 @@ module.exports.run = async (client, message, args, gprefix) => {
     const unable = new MessageEmbed()
         .setDescription('That role is above me.')
         .setColor('RED')
+    const invalid = new MessageEmbed()
+        .setDescription("Provide a color in hex.")
+        .setColor('YELLOW')
     let changed = new MessageEmbed()
 
     let Target = await message.guild.members.fetch(client.user.id)
@@ -18,17 +21,42 @@ module.exports.run = async (client, message, args, gprefix) => {
     if(!message.channel.permissionsFor(Target).has('MANAGE_ROLES')){
         return message.channel.send({embeds: [perms]})
     }
+
+    if(!role) return message.channel.send({embeds: [unfound]})
+
     if(Target.roles.highest.position < role.position){
         return message.channel.send({embeds: [unable]})
     }
 
-    if(!role) return message.channel.send({embeds: [unfound]})
+    if(args.length == 1) return message.channel.send({embeds: [invalid]})
 
-    let roleColor = role.color.toString(16)
-    role.setColor(`#${args[1]}`)
-    let newColor = args[1].toString(16)
+    let roleColor = `#${role.color.toString(16)}`
+    let newColor = args[1].split('#').join('');
+    let random = ((1 << 24) * Math.random() | 0).toString(16)
+    let Color;
+    const reg = /^[0-9A-F]{6}$/i
 
-    changed.setDescription(`Changed the role color for <@&${role.id}> from #${roleColor} \nto #${newColor}`).setColor(`#${args[1]}`)
+    if (!reg.test(newColor)) {
+        newColor = '000000'
+    }
+
+    if(args[1] == 'random'){
+        newColor = random
+        Color = `#${random}`
+        role.setColor(`#${random}`)
+    }else{
+        Color = `#${newColor}`
+        role.setColor(`#${newColor}`)
+    }
+
+    if(roleColor == '#0'){
+        roleColor = 'transparent'
+    }
+    if(newColor == '000000'){
+        Color = `transparent`
+    }
+
+    changed.setDescription(`Changed the role color for <@&${role.id}> from ${roleColor} \nto ${Color}`).setColor(`#${newColor}`)
 
     message.channel.send({embeds: [changed]})
 
