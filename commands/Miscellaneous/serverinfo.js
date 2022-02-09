095e02d6146bb5d137119f3db3f4efd2b611c788
HeartandSoulOfficial <heartandsoulofficial024@gmail.com>
2022-02-08 20:11:39 -0500

    serverinfo command

---
+++ commands/Miscellaneous/serverinfo.js
@@ -0,0 +1,47 @@
+const {discord, MessageEmbed} = require('discord.js')
+const moment = require('moment')
+const fix = require('../../fix')
+
+module.exports.run = async (client, message, args, gprefix) => {
+    let owner = (await message.guild.fetchOwner()).user
+    let categorySize = (await message.guild.channels.fetch()).filter(c => c?.type == 'GUILD_CATEGORY').size
+    let textSize = (await message.guild.channels.fetch()).filter(c => c?.type == 'GUILD_TEXT').size
+    let voiceSize = (await message.guild.channels.fetch()).filter(c => c?.type == 'GUILD_VOICE').size
+    let roleSize = (await message.guild.roles.fetch()).size
+    let memberSize = message.guild.memberCount
+    let boostCount = message.guild.premiumSubscriptionCount
+    let boostTier = fix.fixTier(message.guild.premiumTier)
+    let vanity = fix.fixVanity(message.guild.vanityURLCode)
+    let serverIcon = message.guild.iconURL({dynamic: true})
+    if(serverIcon == null){
+        serverIcon = 'https://techyhost.com/wp-content/uploads/2021/06/Discord-logo.png'
+    }
+
+    let serverinfo = new MessageEmbed()
+    .setAuthor({name: message.guild.name})
+    .addFields(
+        {name: `Categories`, value: `${categorySize}`, inline: true},
+        {name: `Text Channels`, value: `${textSize}`, inline: true},
+        {name: `Voice Channels`, value: `${voiceSize}`, inline: true}
+    )
+    .addFields(
+        {name: `Owner`, value: `${owner.username}#${owner.discriminator}`, inline: true},
+        {name: `Members`, value: `${memberSize}`, inline: true},
+        {name: `Roles`, value: `${roleSize}`, inline: true}
+    )
+    .addFields(
+        {name: `Boosters`, value: `${boostCount}`, inline: true},
+        {name: `Boost Tier`, value: `${boostTier}`, inline: true},
+        {name: `Vanity URL`, value: `${vanity}`, inline: true}
+    )
+    .setThumbnail(serverIcon)
+    .setColor('RANDOM')
+    .setFooter({text: `ID: ${message.guild.id} | Server Created • ${moment(message.guild.createdAt).format('MM/D/YYYY')}`})
+
+    message.channel.send({embeds: [serverinfo]})
+}
+
+module.exports.help = {
+    name: 'serverinfo',
+    aliases: ['server']
+}
--- fix.js
+++ fix.js
@@ -8,6 +8,28 @@ module.exports = {
             return "None"
         } else if(variable == '#000000' || variable == '#0'){
             return "None"
-        }else return variable
-    }
+        } else if(variable == "NONE") {
+            return "None"
+        } else return variable
+    },
+        fixTier: (variable) => {
+            if(variable == "NONE"){
+                return "None"
+            } else {
+                let words = variable.split("_")
+                let combined = []
+                for(j=0; j<words.length; j++){
+                    let word = words[j].toLowerCase()
+                    combined.push(word[0].toUpperCase()+word.substring(1))
+                }
+                return combined.join(" ")
+            }
+    },
+        fixVanity: (variable) => {
+            if(variable == null){
+                return "None"
+            } else{
+                return `https://discord.gg/${variable}`
+            }
+        }
 }
